e9123912d41c3a552af76ef97fb4f997b7dec88f
slyzoo <lilithhimes@gmail.com>
2025-01-16 13:45:49 -0600

    initial Commit

---
+++ src/frameui.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace FrameUI
+{
+  class Window
+  {
+  public:
+    // Window size(X, Y);
+    static int winSize(int a, int b);
+
+    static string winTitle(string a);
+  };
+}
---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+#include "frameui.h"
+
+namespace frameui{}
